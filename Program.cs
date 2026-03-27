22c1414decbac7d7275e64356fda0f8ff9d1856b
Kosovo666 <vladmalysev35@gmail.com>
2026-03-27 11:51:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint oddNumbersCount = 0;
+            uint evenNumbersCount = 0;
+            Console.WriteLine("Введите первое число диапозона");
+            int currentValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последнее число диапозона");
+            int limit = int.Parse(Console.ReadLine());
+            while (currentValue <= limit)
+            {
+                if (currentValue % 2 == 0)
+                {
+                    evenNumbersCount++;
+                }
+                else
+                {
+                    oddNumbersCount++;
+                }
+                currentValue++;
+            }
+            Console.WriteLine("Количество нечётных чисел: " + oddNumbersCount);
+            Console.WriteLine("Количество чётных чисел: " + evenNumbersCount);
+            Console.ReadLine();
+        }
+    }
+}
